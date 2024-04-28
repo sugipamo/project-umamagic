e57214e886e2f2894d4ff5fab740ccf05d0ee3f6
yuunamaniaiiidx@gmail.com <yuunamaniaiiidx@gmail.com>
2024-04-28 14:07:49 +0900

    クッキーデータ生成時にlogin待ち一覧を登録するようにする

--- sources/scraping/models/login_for_scraping.py
+++ sources/scraping/models/login_for_scraping.py
@@ -4,6 +4,28 @@ from selenium import webdriver
 import pickle
 
 COOKIEPATH = "cookies/"
+
+class LoginForScraping(models.Model):
+    domain = models.CharField(max_length=255)
+    loggined = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.domain
+    
+    def login(self, username, password):
+        with WebDriver() as driver:
+            method = getattr(LoginMethods, self.domain.replace(".", ""))
+            method(driver, username, password)
+        self.loggined = True
+        self.save()
+
+    def update_logined(self, driver):
+        method = getattr(LoginMethods, f"logined_{self.domain.replace('.', '')}")
+        self.loggined = method(driver)
+        self.save()
+
 def cookie_required(domain):
     def decorator(func):
         def wrapper(*args, **kwargs):
@@ -23,6 +45,7 @@ def cookie_required(domain):
                 url = "https://" + domain[1:] if domain.startswith(".") else "https://" + domain
                 driver.get(url)
                 [driver.add_cookie(cookie) for cookie in cookies]
+            LoginForScraping.objects.get_or_create(domain=domain)
             return_value = func(*args, **kwargs)
             with open(cookiepath, "wb") as f:
                 cookies = driver.get_cookies()
@@ -63,23 +86,3 @@ class LoginMethods():
             return True
         return False
 
-class LoginForScraping(models.Model):
-    domain = models.CharField(max_length=255)
-    loggined = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.domain
-    
-    def login(self, username, password):
-        with WebDriver() as driver:
-            method = getattr(LoginMethods, self.domain.replace(".", ""))
-            method(driver, username, password)
-        self.loggined = True
-        self.save()
-
-    def update_logined(self, driver):
-        method = getattr(LoginMethods, f"logined_{self.domain.replace('.', '')}")
-        self.loggined = method(driver)
-        self.save()
