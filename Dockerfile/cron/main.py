8cb86a4a583ec773d1a2794d2914cdc9e4d26467
yuunamaniaiiidx@gmail.com <yuunamaniaiiidx@gmail.com>
2024-06-20 18:01:05 +0900

    開発環境の整備 Dockerfileを最低限に build後にイメージ名をつけることでnoneの発生を防ぐ pipfileを削除して代わりにcommands.pyを作成

--- Dockerfile/cron/main.py
+++ Dockerfile/cron/main.py
@@ -1,5 +1,5 @@
 from time import sleep
 import subprocess
 while True:
-    subprocess.Popen("curl django:8000/scraping/doevent/", shell=True)
+    subprocess.Popen("curl django:8000/scraping/doevent/ >> log.txt", shell=True)
     sleep(30)
---
+++ commands.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from pathlib import Path
+import subprocess
+import inspect
+
+COMMANDS = {
+    "up": "docker compose up -d --build",
+    "down": "docker compose down",
+    "restart": "docker compose restart",
+    "logs": "docker compose logs",
+    "exec": "docker compose exec",
+    "gitfetch": "git fetch -p",
+    "migrate": "docker compose exec django bash -c 'python manage.py makemigrations && python manage.py migrate'",
+    "migrate_reset": "docker compose exec django bash -c 'python manage.py migrate --fake && python manage.py migrate'",
+    "start": "docker compose exec django bash -c 'gunicorn --workers 4 --threads 2 umamagic.wsgi:application --bind'",
+    "test_start": "docker compose exec django bash -c 'python manage.py runserver",
+    "test": "docker compose exec django bash -c 'python manage.py test'",
+    "pytest": "docker compose exec django bash -c 'pytest -v -n auto'",    
+}
+
+
+args = sys.argv[1:]
+if not args:
+    args = input("Enter command: ").split()
+
+if not args:
+    print("No command entered")
+    sys.exit()
+
+os.chdir(Path(__file__).parent)
+
+
+subprocess.run(COMMANDS[args[0]].format(*args[1:]), shell=True)
