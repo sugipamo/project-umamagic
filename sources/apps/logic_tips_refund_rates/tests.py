0cf6f6a5d6b513dc8c799ffdc4ca58c879cfaf4f
yuunamaniaiiidx@gmail.com <yuunamaniaiiidx@gmail.com>
2024-08-07 23:14:28 +0900

    払い戻し処理のロジック追加途中

--- sources/apps/logic_tips_refund_rates/tests.py
+++ sources/apps/logic_tips_refund_rates/tests.py
@@ -10,7 +10,40 @@ class RefundRateTestCase(TestCase):
         HorseRacingTip.make_dummy_tips(category='nar', race_id='202444070902')
         HorseRacingTicket.make_dummy_tickets(category='nar', race_id='202444070902')
         tip = HorseRacingTipRefundManager.next()
-        print("tip", tip)
         self.assertTrue(tip is not None)
 
+    def test_calc_combination_count(self):
+        calc_combination_count = HorseRacingTipRefundManager()._HorseRacingTipRefundManager__calc_combination_count
+        def helper(tips):
+            tips = [set(t) for t in tips]
+            from itertools import product
+            cnt = 0
+            # print(tips)
+            for t in product(*tips):
+                if len(set(t)) != len(t):
+                    # print("ng", list(t))
+                    continue
+                # print("ok", list(t))
+                cnt += 1
+            return calc_combination_count(tips), cnt
+        # 3つまで確認
+        self.assertEqual(*helper([[1]]))
+        self.assertEqual(*helper([[1], [1]]))
+        self.assertEqual(*helper([[1, 2], [1, 2]]))
+        self.assertEqual(*helper([[1, 2], [3, 4]]))
+        self.assertEqual(*helper([[1, 2], [2, 3]]))
+        self.assertEqual(*helper([[1, 2], [3, 4], [5, 6]]))
+        self.assertEqual(*helper([[1, 2], [2, 3], [3, 4]]))
+        self.assertEqual(*helper([[1, 2], [3, 4], [5, 6], [7, 8]]))
+        self.assertEqual(*helper([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+        self.assertEqual(*helper([[1, 2, 3], [3, 4, 5], [5, 6, 7]]))
+        self.assertEqual(*helper([[1, 2], [2, 3], [3, 4]]))
+        self.assertEqual(*helper([[1, 2, 3], [2, 3, 4], [3, 4, 5]]))
+
+
+    def test_new_refunds(self):
+        HorseRacingTip.make_dummy_tips(category='nar', race_id='202444070902')
+        HorseRacingTicket.make_dummy_tickets(category='nar', race_id='202444070902')
+        tip = HorseRacingTipRefundManager.new_refunds()
+        self.assertTrue(tip is not None)
 
